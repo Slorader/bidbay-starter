1e88059c9d6d8b304462d6204fa7a4d49afe98c4
Shonned <kilianpeyn@gmail.com>
2024-03-22 09:32:53 +0100

    Api récupération de produit avec un id

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -10,7 +10,22 @@ router.get('/api/products', async (req, res, next) => {
 })
 
 router.get('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  const { productId } = req.params
+
+  try {
+    const product = await Product.findOne({
+      where: { id: productId }
+    })
+
+    if (!product) {
+      return res.status(404).json({ message: 'Produit non trouvé' })
+    }
+
+    res.status(200).json(product)
+  } catch (e) {
+    console.log(e)
+    res.status(500)
+  }
 })
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
