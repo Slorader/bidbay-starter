3ceb01fd06d719914a8e69dc7e52d68f49e9be54
Léo TRUX <truxleo@gmail.com>
2024-03-29 09:37:57 +0100

    recup produit

--- frontend/src/views/EditProductView.vue
+++ frontend/src/views/EditProductView.vue
@@ -6,12 +6,40 @@ import { ref } from "vue";
 const { isAuthenticated, token } = useAuthStore();
 const router = useRouter();
 const route = useRoute();
+const loading = ref(false);
+const error = ref(false);
+const product = ref("");
 
 if (!isAuthenticated.value) {
   router.push({ name: "Login" });
 }
 
 const productId = ref(route.params.productId);
+
+async function fetchProduct() {
+  loading.value = true;
+  error.value = false;
+
+  try {
+    const response = await fetch(
+      "http://localhost:3000/api/products/" + productId.value,
+    );
+    if (!response.ok) {
+      throw new Error("Erreur lors de la récupération des produits");
+    }
+    product.value = await response.json();
+    console.log(product.value);
+    product.value.endDate = new Date(product.value.endDate)
+      .toISOString()
+      .split("T")[0];
+  } catch (e) {
+    console.error("Erreur lors de la récupération du produit:", e);
+    error.value = true;
+  } finally {
+    loading.value = false;
+  }
+}
+fetchProduct();
 </script>
 
 <template>
@@ -20,7 +48,12 @@ const productId = ref(route.params.productId);
   <div class="row justify-content-center">
     <div class="col-md-6">
       <form>
-        <div class="alert alert-danger mt-4" role="alert" data-test-error>
+        <div
+          v-if="error"
+          class="alert alert-danger mt-4"
+          role="alert"
+          data-test-error
+        >
           Une erreur est survenue
         </div>
 
@@ -32,6 +65,7 @@ const productId = ref(route.params.productId);
             id="product-name"
             required
             data-test-product-name
+            v-model="product.name"
           />
         </div>
 
@@ -46,6 +80,7 @@ const productId = ref(route.params.productId);
             rows="3"
             required
             data-test-product-description
+            v-model="product.description"
           ></textarea>
         </div>
 
@@ -57,6 +92,7 @@ const productId = ref(route.params.productId);
             id="product-category"
             required
             data-test-product-category
+            v-model="product.category"
           />
         </div>
 
@@ -74,6 +110,7 @@ const productId = ref(route.params.productId);
               min="0"
               required
               data-test-product-price
+              v-model="product.originalPrice"
             />
             <span class="input-group-text">€</span>
           </div>
@@ -90,6 +127,7 @@ const productId = ref(route.params.productId);
             name="pictureUrl"
             required
             data-test-product-picture
+            v-model="product.pictureUrl"
           />
         </div>
 
@@ -104,18 +142,15 @@ const productId = ref(route.params.productId);
             name="endDate"
             required
             data-test-product-end-date
+            v-model="product.endDate"
           />
         </div>
 
         <div class="d-grid gap-2">
-          <button
-            type="submit"
-            class="btn btn-primary"
-            disabled
-            data-test-submit
-          >
+          <button type="submit" class="btn btn-primary" data-test-submit>
             Modifier le produit
             <span
+              v-if="loading"
               class="spinner-border spinner-border-sm"
               role="status"
               aria-hidden="true"
