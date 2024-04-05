6c29729a436e6867cedd714009e63d6ab03a2470
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-04-05 09:51:18 +0200

    Affichage de la page de profil d'un utilisateur

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,16 +5,26 @@ const router = express.Router()
 
 router.get('/api/users/:userId', async (req, res) => {
   try {
-    const userId = req.params.userId;
-    const user = await User.findAll({
+    const userId = req.params.userId
+    const user = await User.findOne({
+      include: [
+        {
+          model: Product,
+          as: 'products'
+        },
+        {
+          model: Bid,
+          as: 'bids'
+        }
+      ],
       where: {
         id: userId
       }
-    });
+    })
     res.status(200).json(user);
   } catch (error) {
-    console.error('Erreur lors de la récupération des produits :', error);
-    res.status(500).json({ error: 'Erreur lors de la récupération des produits' });
+    console.error('Erreur lors de la récupération de l\'utilisateur :', error);
+    res.status(500).json({ error: 'Erreur lors de la récupération de l\'utilisateur' });
   }
 })
 
